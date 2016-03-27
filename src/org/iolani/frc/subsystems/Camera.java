31dd51066ab1a3d09c444ec3fad6f22b74740018
Jeff Malins <jmalins@iolani.org>
2016-03-27 11:26:28 -1000

    cleared warnings

--- src/org/iolani/frc/subsystems/Camera.java
+++ src/org/iolani/frc/subsystems/Camera.java
@@ -1,9 +1,9 @@
 package org.iolani.frc.subsystems;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.io.OutputStreamWriter;
+//import java.io.PrintWriter;
 
 import org.iolani.frc.RobotMap;
 
