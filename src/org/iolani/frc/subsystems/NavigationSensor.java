ab249b42db8f0c8103b43534d8a1209c5d58689e
Jeff Malins <jmalins@iolani.org>
2016-03-29 07:36:48 -1000

    nav sensor now working

--- src/org/iolani/frc/subsystems/NavigationSensor.java
+++ src/org/iolani/frc/subsystems/NavigationSensor.java
@@ -5,7 +5,8 @@ import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDSourceType;
-import edu.wpi.first.wpilibj.SPI;
+//import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -21,7 +22,8 @@ public class NavigationSensor extends Subsystem implements PIDSource {
             /* Communicate w/navX MXP via the MXP SPI Bus.                                     */
             /* Alternatively:  I2C.Port.kMXP, SerialPort.Port.kMXP or SerialPort.Port.kUSB     */
             /* See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface/ for details. */
-            _ahrs = new AHRS(SPI.Port.kMXP); 
+            //_ahrs = new AHRS(SPI.Port.kMXP);
+			_ahrs = new AHRS(SerialPort.Port.kMXP);
         } catch (RuntimeException ex ) {
             DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
         }
@@ -45,6 +47,7 @@ public class NavigationSensor extends Subsystem implements PIDSource {
 	
 	public void debug() {
 		SmartDashboard.putNumber("imu-yaw", _ahrs.getYaw());
+		SmartDashboard.putBoolean("imu-moving", _ahrs.isMoving());
 		SmartDashboard.putData("imu", _ahrs);
 	}
 	
